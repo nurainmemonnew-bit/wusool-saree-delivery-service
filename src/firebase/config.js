dfb0c2032e5147e42cbca65b05978d2b6fec05a6
Jayasurya005 <jeyasuryav99@gmail.com>
2026-03-27 16:04:03 +0530

    modified firebase config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDjbfK8KvHscYvlJT2vs_RdsG5VHf16uBw",
-  authDomain: "myapp-6be0c.firebaseapp.com",
-  projectId: "myapp-6be0c",
-  storageBucket: "myapp-6be0c.appspot.com",
-  messagingSenderId: "194945113548",
-  appId: "1:194945113548:web:e9604ee9022c47b7c35169",
-  measurementId: "G-WW5YGTCK1Q"
+  apiKey: "AIzaSyA-bCN06t7io4lAFKXHrPnHMW_Hwny5uMg",
+  authDomain: "delivery-service-ba435.firebaseapp.com",
+  projectId: "delivery-service-ba435",
+  storageBucket: "delivery-service-ba435.firebasestorage.app",
+  messagingSenderId: "668185689060",
+  appId: "1:668185689060:web:d2600ff97e88f8491a25f1",
+  measurementId: "G-ZY0YZQ9RJG"
 };
 
 const app = initializeApp(firebaseConfig);
